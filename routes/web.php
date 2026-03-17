277226fd6e4992bddf2222eacb4663efe4473ffc
leegallagher001 <lee.gallagher.writing@outlook.com>
2026-03-17 10:45:09 +0000

    Set up the basics of submitting the sign-up form and verified that the SQLite database is working.

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Route; // 'Route' is a default class of Laravel
 use App\Http\Controllers\LeeBookController;
+use App\Http\Controllers\UserController;
 
 Route::get('/', [LeeBookController::class, "homepage"]); // home page
 Route::get('/about', [LeeBookController::class, "aboutpage"]); // about page
+
+Route::post('/register', [UserController::class, 'register']); // register HTML sign-up form
