b711aa18bd0209d7b22631df3ce6b1244a688bc2
leegallagher001 <lee.gallagher.writing@outlook.com>
2026-03-18 16:07:24 +0000

    Added login functionality, varying the content of the home pageand header bar based on whether a user is logged in or not.

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,6 +8,28 @@ use Illuminate\Validation\Rule;
 
 class UserController extends Controller
 {
+    public function showCorrectHomepage() {
+        if (auth()->check()) {
+            return view('homepage-feed');
+        } else {
+            return view('homepage');
+        }
+    }
+
+    public function login(Request $request) {
+        $incomingFields = $request->validate([
+            'loginusername' => 'required',
+            'loginpassword' => 'required'
+        ]);
+
+        if (auth()->attempt(['username' => $incomingFields['loginusername'], 'password' => $incomingFields['loginpassword']])) {
+            $request->session()->regenerate();
+            return 'Congrats!!!';
+        } else {
+            return 'Sorry';
+        }
+    }
+
     public function register(Request $request) {
         $incomingFields = $request->validate([
             'username' => ['required', 'min:3', 'max:20', Rule::unique('users', 'username')], // 'Rule::unique' is a function and class of input validation that makes sure that the new username is unique (i.e. two people can't set up accounts with the same username)
--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,8 @@ use Illuminate\Support\Facades\Route; // 'Route' is a default class of Laravel
 use App\Http\Controllers\LeeBookController;
 use App\Http\Controllers\UserController;
 
-Route::get('/', [LeeBookController::class, "homepage"]); // home page
+Route::get('/', [UserController::class, "showCorrectHomepage"]); // home page
 Route::get('/about', [LeeBookController::class, "aboutpage"]); // about page
 
 Route::post('/register', [UserController::class, 'register']); // register HTML sign-up form
+Route::post('/login', [UserController::class, 'login']); // login page
