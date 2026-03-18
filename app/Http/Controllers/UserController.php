dd84f137eef5d176d16d19abaff83a0442b7b548
leegallagher001 <lee.gallagher.writing@outlook.com>
2026-03-18 12:33:55 +0000

    Learned about input validation to make limitations on username and password entries (min characters, unique names etc.) as well as Laravel's ability to display error messages if something is entered incorrectly/doesn't meet requirements.

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,14 +4,15 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class UserController extends Controller
 {
     public function register(Request $request) {
         $incomingFields = $request->validate([
-            'username' => 'required',
-            'email' => 'required',
-            'password' => 'required'
+            'username' => ['required', 'min:3', 'max:20', Rule::unique('users', 'username')], // 'Rule::unique' is a function and class of input validation that makes sure that the new username is unique (i.e. two people can't set up accounts with the same username)
+            'email' => ['required', 'email', Rule::unique('users', 'email')],
+            'password' => ['required', 'min:8', 'confirmed']
         ]);
         User::create($incomingFields);
         return 'User information saved!';
