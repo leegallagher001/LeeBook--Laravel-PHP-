a0548969ee60c54c9403fde01026c33a931729ce
leegallagher001 <lee.gallagher.writing@outlook.com>
2026-04-03 16:27:34 +0100

    Dynamic followers and following tabs set up, with ability to see follower and following count as well as being able to access other profiles.

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -49,7 +49,7 @@ class UserController extends Controller
             $currentlyFollowing = Follow::where([['user_id', '=', auth()->user()->id], ['followeduser', '=', $user->id]])->count();
         }
 
-        View::share('sharedData', ['currentlyFollowing' => $currentlyFollowing, 'avatar' => $user->avatar, 'username' => $user->username, 'postCount' => $user->posts()->count()]);
+        View::share('sharedData', ['currentlyFollowing' => $currentlyFollowing, 'avatar' => $user->avatar, 'username' => $user->username, 'postCount' => $user->posts()->count(), 'followerCount' => $user->followers()->count(), 'followingCount' => $user->followingTheseUsers()->count()]);
     }
 
     public function profile(User $user) { // returns the profile posts associated with the user logged in as well as the number of posts that they have in total
@@ -61,13 +61,13 @@ class UserController extends Controller
     public function profileFollowers(User $user) { // returns the profile posts from people that follow the user
         
         $this->getSharedData($user);
-        return view('profile-followers', ['posts' => $user->posts()->latest()->get()]);
+        return view('profile-followers', ['followers' => $user->followers()->latest()->get()]);
     }
 
     public function profileFollowing(User $user) { // returns the profile post from people that the user is following
         
         $this->getSharedData($user);
-        return view('profile-following', ['posts' => $user->posts()->latest()->get()]);
+        return view('profile-following', ['following' => $user->followingTheseUsers()->latest()->get()]);
     }
 
     public function logout() {
